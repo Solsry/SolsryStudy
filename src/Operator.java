8aa5787661f1b3e2ce459ce79801b6b1041b1b35
solsry <qkrwlsthf147@gmail.com>
2023-09-21 17:27:45 +0900

    01

---
+++ src/Operator.java
@@ -0,0 +1,12 @@
+public class Operator {
+    public static void main(String[] args) {
+        int kor, eng, sum, evg;
+        kor = 77 ;
+        eng = 87 ;
+        sum = kor + eng ;
+        evg = (kor + eng) / 2 ;
+
+        System.out.println(sum);
+        System.out.println(evg);
+    }
+}
