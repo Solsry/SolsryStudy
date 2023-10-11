be89800fad2ae3e9f1fdf566f41300008e7386ca
solsry <qkrwlsthf147@gmail.com>
2023-10-11 16:16:54 +0900

    :)

--- src/solsry/java/part02/PersonAccessTest.java
+++ src/solsry/java/part02/PersonAccessTest.java
@@ -1,4 +1,12 @@
 package solsry.java.part02;
 
 public class PersonAccessTest {
+    public static void main(String[] args) {
+        // Q. Person클래스에 이름, 나이, 전화번호를 저장하고 출력하세요.
+        Person p = new Person();
+        p.name = "solsry";
+        p.age = 1000; // ?
+        p.phone = "010-9999-9999";
+        System.out.println(p.name+"\t"+p.age+"\t"+p.phone);
+    }
 }
