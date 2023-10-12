373d8ecbeff7f6698c3ffed83d8f5afbb7b16d04
solsry <qkrwlsthf147@gmail.com>
2023-10-12 19:21:02 +0900

    :)

---
+++ src/solsry/java/poly/Animal.java
@@ -0,0 +1,13 @@
+package solsry.java.poly;
+
+// 추상 클래스 (abstract) -> 불완전한 클래스
+public abstract class Animal {
+    // public void eat();
+    // 추상 메서드( 불완전한 메서드) : 메서드의 구현부(body)가 없다.
+    public abstract void eat();
+
+    // 구현 메서드
+    public void move(){
+        System.out.println("무리를 지어 이동한다.");
+    }
+}
---
+++ src/solsry/java/poly/Cat.java
@@ -0,0 +1,12 @@
+package solsry.java.poly;
+
+public class Cat extends Animal{
+    public void night(){
+        System.out.println("밤에 눈에서 빛난다..");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("고양이 처럼 먹다.");
+    }
+}
